abb1aa9e39e9d09f88a3483661235626f2cb954d
eelitt <ee.littow@gmail.com>
2020-04-09 21:04:22 +0300

    vahitellen..

--- src/main/java/com/example/pelaajaapp/luoPelaajaIkkuna.java
+++ src/main/java/com/example/pelaajaapp/luoPelaajaIkkuna.java
@@ -3,14 +3,39 @@ package com.example.pelaajaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static android.view.View.VISIBLE;
 
 public class luoPelaajaIkkuna extends AppCompatActivity {
+
+    Button mmrToggleButton;
+    EditText asetammrLaatikko;
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_luo_pelaaja_ikkuna);
     }
+
+    public void showTextField(View view) {
+
+
+                mmrToggleButton = findViewById(R.id.MmrToggleButton);
+                asetammrLaatikko = findViewById(R.id.asetaMmrLaatikko);
+                asetammrLaatikko.setVisibility(VISIBLE);
+                counter++;
+                if(counter == 2)
+                {
+                    asetammrLaatikko.setVisibility(View.INVISIBLE);
+                    counter = 0;
+                }
+            }
+
+
     public void luoPelaaja()
     {
     Pelaaja player;
@@ -18,3 +43,4 @@ public class luoPelaajaIkkuna extends AppCompatActivity {
     }
 
 }
+
