2f1f12906f941702c38c1bc9c7c7e0ce0ccc3283
eelitt <ee.littow@gmail.com>
2020-04-15 10:59:06 +0300

    huhhuh

--- src/main/java/com/example/pelaajaapp/MyAdapter.java
+++ src/main/java/com/example/pelaajaapp/MyAdapter.java
@@ -3,49 +3,150 @@ package com.example.pelaajaapp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.TextView;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
+public class MyAdapter extends SelectableAdapter<MyAdapter.MyViewHolder> {
+
+    private static final String TAG = MyAdapter.class.getSimpleName();
+    private static final int TYPE_INACTIVE = 0;
+    private static final int Type_ACTIVE = 1;
 private ArrayList<Pelaaja> apuPelaajalista;
+private MyViewHolder.ClickListener clickListener;
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
-       public TextView apuNimilaatikko;
-        public TextView apuMmrasetaLaatikko;
 
-        public MyViewHolder(View v) {
-            super(v);
+public MyAdapter(ArrayList<Pelaaja> omaPelaajalista, MyViewHolder.ClickListener clicklistener){
+    super();
+    apuPelaajalista = omaPelaajalista;
 
-            apuNimilaatikko =  v.findViewById(R.id.nameBox);
-            apuMmrasetaLaatikko = v.findViewById(R.id.ratingBox);
+    this.clickListener = clicklistener;
+}
+    public void removePlayer(int position) {
+        apuPelaajalista.remove(position);
+        notifyItemRemoved(position);
+    }
+    public void removePlayers(List<Integer> positions){
+
+        Collections.sort(positions, new Comparator<Integer>(){
+            @Override
+            public int compare(Integer lhs, Integer rhs) {
+                return rhs-lhs;
+            }
+        });
+        while(!positions.isEmpty()){
+            if(positions.size() == 1){
+                removePlayer(positions.get(0));
+                positions.remove(0);
+            }else{
+                int count = 1;
+                while(positions.size() > count && positions.get(count).equals(positions.get(count - 1)-1)){
+                    ++count;
+                }
+                if (count == 1){
+                    removePlayer(positions.get(0));
+                }else{
+                    removeRange(positions.get(count -1), count);
+                }
+                for(int i =0; i < count; ++i) {
+                    positions.remove(0);
+                }
+
+            }
         }
-    }
-    public MyAdapter(ArrayList<Pelaaja> omaPelaajalista){
-        apuPelaajalista = omaPelaajalista;
-    }
 
+    }
+    private void removeRange(int positionStart, int itemCount){
+        for (int i = 0; i <itemCount; ++i){
+            apuPelaajalista.remove(positionStart);
+        }
+        notifyItemRangeRemoved(positionStart, itemCount);
+    }
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
-        View va =  LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_player, parent, false);
-        MyViewHolder mvh = new MyViewHolder(va);
-        return mvh;
+        final int layout = viewType == TYPE_INACTIVE ? R.layout.activity_player : R.layout.pelaaja_active;
+        View v = LayoutInflater.from(parent.getContext()).inflate(layout, parent, false);
+        return new MyViewHolder(v, clickListener);
     }
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position){
-        Pelaaja currentPelaaja = apuPelaajalista.get(position);
+
+        final Pelaaja currentPelaaja = apuPelaajalista.get(position);
         holder.apuNimilaatikko.setText(currentPelaaja.getNimi());
         holder.apuMmrasetaLaatikko.setText(String.valueOf(currentPelaaja.getMmr()));
+        holder.selectedOverlay.setVisibility(isSelected(position) ? View.VISIBLE : View.INVISIBLE);
     }
     @Override
 
     public int getItemCount(){
         return apuPelaajalista.size();
     }
+
+    @Override
+    public int getItemViewType(int position){
+
+        final Pelaaja currentPelaaja = apuPelaajalista.get(position);
+        return currentPelaaja.isActive() ? Type_ACTIVE : TYPE_INACTIVE;
+
+    }
+
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+
+       public TextView apuNimilaatikko;
+        public TextView apuMmrasetaLaatikko;
+        View selectedOverlay;
+        private ClickListener listener;
+
+        public MyViewHolder(View v, ClickListener listener) {
+            super(v);
+
+            apuNimilaatikko =  v.findViewById(R.id.nameBox);
+            apuMmrasetaLaatikko = v.findViewById(R.id.ratingBox);
+            selectedOverlay = v.findViewById(R.id.selected_overlay);
+
+            this.listener = listener;
+
+            v.setOnClickListener(this);
+            v.setOnLongClickListener(this);
+
+        }
+        @Override
+        public void onClick(View v){
+        if(listener != null){
+             listener.onItemClicked(getLayoutPosition());
+        }
+
+        }
+        @Override
+        public boolean onLongClick(View v){
+            if(listener != null){
+              return listener.onItemLongClicked(getLayoutPosition());
+        }
+            return false;
+    }
+    public interface ClickListener {
+        void onItemClicked(int position);
+        boolean onItemLongClicked(int position);
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
 
 }
--- src/main/java/com/example/pelaajaapp/luoPelaajaIkkuna.java
+++ src/main/java/com/example/pelaajaapp/luoPelaajaIkkuna.java
@@ -3,47 +3,32 @@ package com.example.pelaajaapp;
         import androidx.appcompat.app.AppCompatActivity;
         import androidx.recyclerview.widget.LinearLayoutManager;
         import androidx.recyclerview.widget.RecyclerView;
-        import java.io.BufferedReader;
-        import java.io.EOFException;
-        import java.io.File;
-        import java.io.FileNotFoundException;
-        import java.io.FileOutputStream;
-        import java.io.FileInputStream;
-        import java.io.IOException;
-        import java.io.InputStreamReader;
-        import java.io.ObjectInputStream;
-        import java.io.ObjectOutput;
-        import java.io.ObjectOutputStream;
-        import java.io.Serializable;
         import java.lang.reflect.Type;
         import java.util.ArrayList;
-        import java.util.List;
-        import android.content.Context;
         import android.content.SharedPreferences;
         import android.os.Bundle;
         import android.util.Log;
+        import android.view.ActionMode;
+        import android.view.Menu;
+        import android.view.MenuItem;
         import android.view.View;
-
         import android.widget.EditText;
-        import android.widget.Toast;
         import android.widget.ToggleButton;
-
-
         import com.google.gson.Gson;
         import com.google.gson.reflect.TypeToken;
-
         import static android.view.View.VISIBLE;
 
-public class luoPelaajaIkkuna extends AppCompatActivity {
-    //private static final String FILE_NAME = "pelaajalista.srl";
+public class luoPelaajaIkkuna extends AppCompatActivity implements MyAdapter.MyViewHolder.ClickListener {
+
     ArrayList<Pelaaja> pelaajalista = new ArrayList<Pelaaja>();
    ToggleButton mmrToggleButton;
     EditText asetammrLaatikko;
 
-    EditText temploadlaatikko;
     RecyclerView listalaatikko;
-    private RecyclerView.Adapter adapteri;
     private RecyclerView.LayoutManager layoutManager;
+    private MyAdapter myadapter;
+    private ActionModeCallback actionModeCallback = new ActionModeCallback();
+    private ActionMode actionmode;
 
 
     int counter = 0;
@@ -52,13 +37,74 @@ public class luoPelaajaIkkuna extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_luo_pelaaja_ikkuna);
-
+        load();
         listalaatikko = findViewById(R.id.listaLaatikko);
         listalaatikko.setHasFixedSize(true);
        layoutManager = new LinearLayoutManager(this);
-        adapteri = new MyAdapter(pelaajalista);
+        myadapter = new MyAdapter(pelaajalista,this);
+         listalaatikko.setAdapter(myadapter);
         listalaatikko.setLayoutManager(layoutManager);
-        listalaatikko.setAdapter(adapteri);
+
+    }
+
+    @Override
+    public void onItemClicked(int position){
+        if(actionmode != null){
+            toggleSelection(position);
+        }else{
+            myadapter.removePlayer(position);
+        }
+    }
+    @Override
+    public boolean onItemLongClicked(int position){
+        if(actionmode == null) {
+            actionmode = startActionMode(actionModeCallback);
+        }
+        toggleSelection(position);
+        return true;
+    }
+    private void toggleSelection(int position) {
+        myadapter.toggleSelection(position);
+        int count = myadapter.getSelectedItemCount();
+        if(count == 0){
+            actionmode.finish();
+        }else{
+            actionmode.setTitle(String.valueOf(count));
+            actionmode.invalidate();
+        }
+
+    }
+
+    private class ActionModeCallback implements ActionMode.Callback {
+
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu){
+            mode.getMenuInflater().inflate(R.menu.selected_menu, menu);
+
+            return true;
+        }
+@Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu){
+            return false;
+}
+
+            @Override
+                    public void onDestroyActionMode(ActionMode mode) {
+                myadapter.clearSelection();
+                actionmode = null;
+    }
+    @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item){
+            switch(item.getItemId()) {
+                case R.id.menu_remove:
+                myadapter.removePlayers(myadapter.getSelectedPlayers());
+                mode.finish();
+                return true;
+
+                default:
+                    return false;
+            }
+    }
 
     }
 
@@ -74,17 +120,17 @@ public class luoPelaajaIkkuna extends AppCompatActivity {
         }
     }
     public void save(View view) {
-        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        SharedPreferences sharedPreferences = getSharedPreferences("com.example.PelaajaApp", MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         Gson gson = new Gson();
         String json = gson.toJson(pelaajalista);
-        editor.putString("task lista", json);
+        editor.putString("pelaajalista", json);
         editor.apply();
     }
-    public void load() {
-        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+    private void load() {
+        SharedPreferences sharedPreferences = getSharedPreferences("com.example.PelaajaApp", MODE_PRIVATE);
         Gson gson = new Gson();
-        String json = sharedPreferences.getString("pelaajalista", null);
+        String json = sharedPreferences.getString("pelaajalista",null);
         Type type = new TypeToken<ArrayList<Pelaaja>>() {}.getType();
         pelaajalista = gson.fromJson(json, type);
 
@@ -92,58 +138,6 @@ public class luoPelaajaIkkuna extends AppCompatActivity {
             pelaajalista = new ArrayList<>();
         }
     }
-
-/*public void save(Pelaaja player)
-{
-
-    //ObjectOutput out = null;
-try{
-    FileOutputStream fos = openFileOutput(FILE_NAME,MODE_PRIVATE | MODE_APPEND);
-
-    ObjectOutput out = new ObjectOutputStream(fos);
-    out.writeObject(player);
-    out.flush();
-    Toast.makeText(this, "Saved to "+ getFilesDir()+ "/" + FILE_NAME, Toast.LENGTH_LONG).show();
-
-    nimiLaatikko.getText().clear();
-
-
-} catch (IOException e) {
-    e.printStackTrace();
-}
-
-}
-
-public void load(View view) {
-
-    Pelaaja player;
-
-    try {
-        FileInputStream fis = openFileInput(FILE_NAME);
-        //ObjectInputStream ois = new ObjectInputStream(fis);
-        StringBuilder sb = new StringBuilder();
-        Log.i("avail",String.valueOf(fis.available()));
-        while(fis.available() >= 0){
-            ObjectInputStream ois = new ObjectInputStream(fis);
-            player = (Pelaaja) ois.readObject();
-            sb.append("nimi: ");
-            sb.append(player.getNimi());
-            sb.append(" rating: ");
-            sb.append(player.getMmr());
-            sb.append(" ");
-
-            temploadlaatikko = (EditText) findViewById(R.id.loadLaatikko);
-            temploadlaatikko.setText(sb.toString());
-            Log.i("while read file", String.valueOf(sb));
-
-        }
-        fis.close();
-
-        } catch (IOException | ClassNotFoundException e) {
-        e.printStackTrace();
-    }
-
-}*/
 
     public void luoPelaaja(View view) {
 
@@ -174,8 +168,9 @@ public void load(View view) {
 
         pelaajalista.add(new Pelaaja(name, ratinki));
         Log.i("maara", String.valueOf(pelaajalista.size()));
-        adapteri.notifyItemInserted(pelaajalista.size());
+
     }
+
 
 
 }
